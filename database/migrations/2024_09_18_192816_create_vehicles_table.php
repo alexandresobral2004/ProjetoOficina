3ba108636bd81b9defcb19833214ddd28f6b873e
everton-dk9 <everyondk9@gmail.com>
2024-09-19 17:17:26 -0300

    migration e model para entidade Vehicle (veiculo)

---
+++ database/migrations/2024_09_18_192816_create_vehicles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('model', 100);
+            $table->string('plate_number', 7)->unique();
+            $table->year('year_of_fabrication');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
