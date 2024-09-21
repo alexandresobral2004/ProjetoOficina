d7d55384e3feb7c09ad2791f8b42c53f6991bb7a
Alexandre Rocha <alexandresobral2004@gmail.com>
2024-09-20 23:32:04 -0300

    correcao header

--- resources/views/layouts/navbars/header.blade.php
+++ resources/views/layouts/navbars/header.blade.php
@@ -172,15 +172,20 @@
                              <img class="rounded-circle header-profile-user"
                                  src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="Header Avatar">
                              <span class="text-start ms-xl-2">
-                                 <span
-                                     class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ Auth::user()->name }}</span>
+                                 <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">
+                                     @if (!is_null(Auth::user()->name))
+                                         <h6 class="dropdown-header">Welcome {{ Auth::user()->name }}</h6>
+                                     @endif
+                                 </span>
                                  <span class="d-none d-xl-block ms-1 fs-12 user-name-sub-text">Founder</span>
                              </span>
                          </span>
                      </button>
                      <div class="dropdown-menu dropdown-menu-end">
                          <!-- item-->
-                         <h6 class="dropdown-header">Welcome {{ Auth::user()->name }}</h6>
+                         @if (!is_null(Auth::user()->name))
+                             <h6 class="dropdown-header">Welcome {{ Auth::user()->name }}</h6>
+                         @endif
                          <a class="dropdown-item" href="pages-profile.html"><i
                                  class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
                                  class="align-middle">Profile</span></a>
